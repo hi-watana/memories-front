3d366dd2dff1600b80e1973989e6d359c59f7bc2
hi-watana <hero95alive@gmail.com>
2021-07-07 22:40:25 +0900

    Comment out unused imports

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -6,9 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+//import Button from '@material-ui/core/Button';
+//import IconButton from '@material-ui/core/IconButton';
+//import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import 'typeface-roboto'
 
